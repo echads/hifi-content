76584a8fd655b5bf6b61cfc915d6cb4d3b20e1d1
echads <37284476+echads@users.noreply.github.com>
2022-02-25 18:46:08 -0800

    Update gasMainButton.js

--- DomainContent/Zombies/gasMainButton.js
+++ DomainContent/Zombies/gasMainButton.js
@@ -11,7 +11,7 @@
 (function(){
     var FIRE_BY_GAS_MAIN_1 = "{358ac501-b964-4502-a0d6-2be2be3691cd}";
     var FIRE_BY_GAS_MAIN_2 = "{21c65586-06c0-429b-9dc3-2a13d7e92a10}";
-    var FIRE_BY_GENERATOR = "{84d98c70-fe1c-4624-a8ba-d0e9171a1e46}";
+    var FIRE_BY_GENERATOR = "{12067fc3-4b43-467a-8bbb-cd8a86a78ad6}";
     var FIRE_BY_CAFE = "{88aec9a6-97cc-4828-9983-493ab81f9131}";
     var MAX_YAW = 0.9;
     var ANGULAR_VELOCITY = {
